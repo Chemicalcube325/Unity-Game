296f1053228cf66b064c5db92e36ccd1b5ee9086
Chemicalcube325 <JacobGutierrez325@gmail.com>
2023-12-02 22:44:21 +0800

    Complete Game

---
+++ Lapu-Lapu's Adventure (Finished Level 3)/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+
+    private TextMeshProUGUI cherriesText;
+
+    [SerializeField] private AudioSource pickUpSound;
+
+    private DataManager dataManager;
+
+    public void Awake()
+    {
+        GameObject textObject = GameObject.Find("Cherries Text");
+        GameObject dataManagerObject = GameObject.Find("Datamanger");
+
+        if (textObject != null)
+        {
+            cherriesText = textObject.GetComponent<TextMeshProUGUI>();
+        }
+
+        dataManager = GameObject.FindObjectOfType<DataManager>();
+
+        if (dataManagerObject != null)
+        {
+            Debug.Log("Datamanager not found in the secne.");
+        }
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            pickUpSound.Play();
+            Destroy(collision.gameObject);
+            cherries++;
+            cherriesText.text = "Coins: " + cherries;
+        }
+
+        if (collision.gameObject.CompareTag("Checkpoint"))
+        {
+            dataManager.AddToPersistentList(cherries);
+            dataManager.MergeSortPersistentList();
+        }
+    }
+}
